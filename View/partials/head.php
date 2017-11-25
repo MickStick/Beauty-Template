358fc2c9fb5fe01f7121952e49802776722fa379
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-11-25 11:15:45 -0500

    Changed favicon.

--- View/partials/head.php
+++ View/partials/head.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Beauty Template <?php echo $title; ?></title>
-    <link rel="shortcut icon" href="/Public/Static/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/Public/Static/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="/Public/Style/Style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
