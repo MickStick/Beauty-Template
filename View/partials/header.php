3ef2b4356351371cd444df7c3261849662990082
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-11-29 08:44:27 -0500

    Added new style to side-nav.

--- View/partials/header.php
+++ View/partials/header.php
@@ -8,10 +8,10 @@
                 <li><a href="/about" >Acme</a></li>
                 <li><a href="/services">Services</a></li>
             </ul>
-            <ul class="side-nav " id="mobile-demo">
-                <li><a href="/" >Home</a></li>
-                <li><a href="/about" >Acme</a></li>
-                <li><a href="/services">Services</a></li>
+            <ul class="side-nav transparent" id="mobile-demo">
+                <li><a class="white-text" href="/" >Home</a></li>
+                <li><a class="white-text" href="/about" >Acme</a></li>
+                <li><a class="white-text" href="/services">Services</a></li>
             </ul>
         </div>
     </nav>
